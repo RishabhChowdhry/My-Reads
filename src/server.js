ec1ebda13ebc0d25e87b064ef29537f91f0d0de5
RishabhChowdhry <86976060+RishabhChowdhry@users.noreply.github.com>
2021-08-17 01:51:24 +0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ app.use("/", express.static(__dirname + "/build"));
 app.get("/", (req, res) => res.sendFile(__dirname + "/build/index.html"));
 
 mongoose.connect(
-  process.env.MONGODB_URL || "mongodb://localhost/react-shopping-cart-db",
+  process.env.MONGODB_URL || "mongodb://localhost/react-myreads-cart-db",
   {
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -26,7 +26,7 @@ const Product = mongoose.model(
     description: String,
     image: String,
     price: Number,
-    availableSizes: [String],
+    availableGenre: [String],
   })
 );
 
